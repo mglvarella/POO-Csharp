af63390742befb70a13c8d96915d34e224237037
Miguel Varella <mig-varella1@hotmail.com>
2023-03-26 17:42:30 -0300

    Atualizacao do exercicio do triangulo

--- TrianguloPOO/TrianguloPOO/Triangulo.cs
+++ TrianguloPOO/TrianguloPOO/Triangulo.cs
@@ -10,5 +10,11 @@ namespace TrianguloPOO {
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));  
+        }
     }
 }
